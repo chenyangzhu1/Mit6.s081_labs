85f29f373d375ceca6cde238d66df4181fe5057c
200111511 <200111511@comp0.hwccluster.labcenter>
2022-09-06 01:36:17 +0000

    test

--- user/sleep.c
+++ user/sleep.c
@@ -2,6 +2,9 @@
 #include "user.h"
 
 int main(int argc,char* argv[]){
+    //argc表示参数数量
+    //argv存储字符串指针
+    //例如在执行命令“cp file_a file_b”时，程序cp的main函数接收到的参数为：argc=3，argv=["cp", "file_a", "file_b"，null]。
     if(argc != 2){
         printf("Sleep needs one argument!\n"); //检查参数数量是否正确
         exit(-1);
