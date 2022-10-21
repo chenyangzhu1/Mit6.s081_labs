6be651a9bf86088cf09425d61a373f97cfd6b917
chenyangzhu1 <chenyangzhu.cs@foxmail.com>
2022-10-21 11:25:43 +0000

    lab3代码小修

--- kernel/bio.c
+++ kernel/bio.c
@@ -13,7 +13,6 @@
 // * Only one process at a time can use a buffer,
 //     so do not keep them longer than necessary.
 
-
 #include "types.h"
 #include "param.h"
 #include "spinlock.h"
@@ -27,18 +26,19 @@
 //根据指导书的说法定义为13，但感觉定义为23 29，会有更好的效果
 
 /*依据hint
-It is OK to use a fixed number of buckets and not resize the hash table dynamically. 
+It is OK to use a fixed number of buckets and not resize the hash table dynamically.
 Use a prime number of buckets (e.g., 13) to reduce the likelihood of hashing conflicts.
 */
-struct {
+struct
+{
   struct spinlock lock;
   struct buf head;
 } buckets[NBUC];
 //桶结构   数组
 /*各自的bcaches[i].head维护一条双向链表*/
 
-
-struct {
+struct
+{
   struct spinlock lock;
   struct buf buf[NBUF];
 
@@ -49,44 +49,39 @@ struct {
   //去掉head，因为我们这里为每个桶上锁，因此不再需要head，直接使用bucket中的head即可
 } bcache;
 
-
 /*将buffer cache分为若干桶，每个桶加一个单独的锁来保护*/
-/*题目还希望我们利用buffer被调用的时间戳来判断该buffer
-在buffer cache中是否有留存的必要，这一点题目提示中指引我们使用ticks变量*/
-void
-binit(void)
-{//初始化
+
+void binit(void)
+{ //初始化
   struct buf *b;
 
   initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
-  for(int i=0;i<NBUC;i++)
+  for (int i = 0; i < NBUC; i++)
   {
-    initlock(&buckets[i].lock,"bcache.buckets");
+    initlock(&buckets[i].lock, "bcache.buckets");
     //除了bcache本身的锁以外
     //所有的13个桶也要上锁
-    buckets[i].head.prev=&buckets[i].head;
-    buckets[i].head.next=&buckets[i].head;
+    buckets[i].head.prev = &buckets[i].head;
+    buckets[i].head.next = &buckets[i].head;
     //上锁方式仿照原本的bcache，就下面被注释的两行
   }
 
-
-
-
   // bcache.head.prev = &bcache.head;
   // bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    int hashnum=b->blockno%NBUC;
-    b->next=buckets[hashnum].head.next;
-    b->prev=&buckets[hashnum].head;
+  for (b = bcache.buf; b < bcache.buf + NBUF; b++)
+  {
+    int hashnum = b->blockno % NBUC;
+    b->next = buckets[hashnum].head.next;
+    b->prev = &buckets[hashnum].head;
 
     // b->next = bcache.head.next;
     // b->prev = &bcache.head;
     initsleeplock(&b->lock, "buffer");
-    buckets[hashnum].head.next->prev=b;
-    buckets[hashnum].head.next=b;
-    
+    buckets[hashnum].head.next->prev = b;
+    buckets[hashnum].head.next = b;
+
     // bcache.head.next->prev = b;
     // bcache.head.next = b;
   }
@@ -95,19 +90,19 @@ binit(void)
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
-static struct buf*
+static struct buf *
 bget(uint dev, uint blockno)
-{/*为device dev返回一个序号为blockno的buffer，并更新其在buffer cache中的位置。*/
+{ /*为device dev返回一个序号为blockno的buffer，并更新其在buffer cache中的位置。*/
   struct buf *b;
-  int hashnum=blockno%NBUC;
+  int hashnum = blockno % NBUC;
   //直接用序号对桶的个数取模，作为准备进入的哈希桶的序号
   //这一段代码就是仿照下一段代码进行修改，把进入bcache改成进入哈希桶，基本原理都是一样的
   // Is the block already cached?
-  acquire(&buckets[hashnum].lock);//先获取锁
-  for(b=buckets[hashnum].head.next;b!=&buckets[hashnum].head;b=b->next)
-  {//利用head进行链表遍历
-    if(b->dev==dev && b->blockno==blockno)
-    {//找到对应的blockno，则标记引用，并返回
+  acquire(&buckets[hashnum].lock); //先获取锁
+  for (b = buckets[hashnum].head.next; b != &buckets[hashnum].head; b = b->next)
+  { //利用head进行链表遍历
+    if (b->dev == dev && b->blockno == blockno)
+    { //找到对应的blockno，则标记引用，并返回
       b->refcnt++;
       release(&buckets[hashnum].lock);
       acquiresleep(&b->lock);
@@ -127,53 +122,52 @@ bget(uint dev, uint blockno)
   //   }
   // }
 
-
   //如果没有命中，第一种想法是不麻烦别人，自己先考虑解决
   //简单的说就是看看自己的桶里面有没有空着的块，也就是refcnt=0，把它拿过来，修改成现在我们需要的块即可
-  for(b=buckets[hashnum].head.next;b!=&buckets[hashnum].head;b=b->next)
+  for (b = buckets[hashnum].head.next; b != &buckets[hashnum].head; b = b->next)
   {
-    if(b->refcnt==0)
+    if (b->refcnt == 0)
     {
-        b->dev = dev;
-        b->blockno = blockno;
-        b->valid = 0;
-        b->refcnt = 1;//把这一块改成我们需要的快，并返回
-        release(&buckets[hashnum].lock);
-        acquiresleep(&b->lock);
-        return b;
+      b->dev = dev;
+      b->blockno = blockno;
+      b->valid = 0;
+      b->refcnt = 1; //把这一块改成我们需要的块，并返回
+      release(&buckets[hashnum].lock);
+      acquiresleep(&b->lock);
+      return b;
     }
   }
   //自己也没找到，那没办法，只能问别人借
   //直接去别的桶里面找
-  for(int i=0;i<NBUC;i++)
+  for (int i = 0; i < NBUC; i++)
   {
-    if(i!=hashnum)//确保是进了别人的桶
+    if (i != hashnum) //确保是进了别人的桶
     {
       acquire(&buckets[i].lock);
-      for(b=buckets[i].head.prev;b!=&buckets[i].head;b=b->prev)//遍历桶中的每个块
+      for (b = buckets[i].head.prev; b != &buckets[i].head; b = b->prev) //遍历桶中的每个块
       {
-        if(b->refcnt==0)//如果找到了一个没被占用的块
+        if (b->refcnt == 0) //如果找到了一个没被占用的块
         {
           b->dev = dev;
           b->blockno = blockno;
           b->valid = 0;
-          b->refcnt = 1;//首先把这个块改造成我们需要的样子
+          b->refcnt = 1; //首先把这个块改造成我们需要的样子
           b->next->prev = b->prev;
-          b->prev->next = b->next;//把这个b从原来的链表中断开
-          b->next=buckets[hashnum].head.next;
-          b->prev=&buckets[hashnum].head;
-          buckets[hashnum].head.next->prev=b;
-          buckets[hashnum].head.next=b;//把这个b放到我们原本的桶的链表表头
+          b->prev->next = b->next; //把这个b从原来的链表中断开
+          b->next = buckets[hashnum].head.next;
+          b->prev = &buckets[hashnum].head;
+          buckets[hashnum].head.next->prev = b;
+          buckets[hashnum].head.next = b; //把这个b放到我们原本的桶的链表表头
           release(&buckets[hashnum].lock);
-          release(&buckets[i].lock);//这个部分我们其实同时获取了两把锁
+          release(&buckets[i].lock); //这个部分我们其实同时获取了两把锁
           acquiresleep(&b->lock);
-          return b;//返回b
+          return b; //返回b
         }
       }
-      release(&buckets[i].lock);//也有可能到这里还是没找到，还得解锁
+      release(&buckets[i].lock); //也有可能到这里还是没找到，还得解锁
     }
   }
-  release(&buckets[hashnum].lock);//也有可能到这里还是没找到，还得解锁
+  release(&buckets[hashnum].lock); //也有可能到这里还是没找到，还得解锁
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
@@ -189,19 +183,20 @@ bget(uint dev, uint blockno)
   //     直到wakeup()唤醒后，获取到锁，并将b.lock置1*/
   //     return b;
   //     //找到别的地方的空闲区，返回之
-    // }
+  // }
   // }
   panic("bget: no buffers");
 }
 
 // Return a locked buf with the contents of the indicated block.
-struct buf*
+struct buf *
 bread(uint dev, uint blockno)
 {
   struct buf *b;
 
   b = bget(dev, blockno);
-  if(!b->valid) {
+  if (!b->valid)
+  {
     virtio_disk_rw(b, 0);
     b->valid = 1;
   }
@@ -209,35 +204,34 @@ bread(uint dev, uint blockno)
 }
 
 // Write b's contents to disk.  Must be locked.
-void
-bwrite(struct buf *b)
+void bwrite(struct buf *b)
 {
-  if(!holdingsleep(&b->lock))
+  if (!holdingsleep(&b->lock))
     panic("bwrite");
   virtio_disk_rw(b, 1);
 }
 
 // Release a locked buffer.
 // Move to the head of the most-recently-used list.
-void
-brelse(struct buf *b)
-{/*它实现的是告知buffer cache某个buffer不再被设备调用，
-实现方式在于将该buffer对应的refcnt减一，
-而当这个refcnt被减小到0时，则没有任何调用该buffer的设备，
-它处于空闲状态，于是要将这个buffer从buffer cache中主动移到链表最前位，
-从而实现对buffer cache双向链表性质的维护。
-*/
-  if(!holdingsleep(&b->lock))
+void brelse(struct buf *b)
+{ /*它实现的是告知buffer cache某个buffer不再被设备调用，
+ 实现方式在于将该buffer对应的refcnt减一，
+ 而当这个refcnt被减小到0时，则没有任何调用该buffer的设备，
+ 它处于空闲状态，于是要将这个buffer从buffer cache中主动移到链表最前位，
+ 从而实现对buffer cache双向链表性质的维护。
+ */
+  if (!holdingsleep(&b->lock))
     panic("brelse");
 
   releasesleep(&b->lock);
-  int hashnum=b->blockno%NBUC;
+  int hashnum = b->blockno % NBUC;
 
   // acquire(&bcache.lock);
   //修改成对应的bucket操作即可
   acquire(&buckets[hashnum].lock);
   b->refcnt--;
-  if (b->refcnt == 0) {
+  if (b->refcnt == 0)
+  {
     // no one is waiting for it.
     b->next->prev = b->prev;
     b->prev->next = b->next;
@@ -250,23 +244,21 @@ brelse(struct buf *b)
   // release(&bcache.lock);
 }
 
-void
-bpin(struct buf *b) {
+void bpin(struct buf *b)
+{
   //修改成对应的bucket操作即可
-  int hashnum=b->blockno%NBUC;
+  int hashnum = b->blockno % NBUC;
   acquire(&buckets[hashnum].lock);
   b->refcnt++;
   release(&buckets[hashnum].lock);
 }
 
-void
-bunpin(struct buf *b) {
+void bunpin(struct buf *b)
+{
   //修改成对应的bucket操作即可
-  int hashnum=b->blockno%NBUC;
+  int hashnum = b->blockno % NBUC;
 
   acquire(&buckets[hashnum].lock);
   b->refcnt--;
   release(&buckets[hashnum].lock);
 }
-
-
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,34 +14,34 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
-} kmem[NCPU];//我们需要多个锁
+} kmem[NCPU]; //我们需要多个锁
 
-void
-kinit()//由于我们拥有了多个锁，自然需要对多个锁同时进行初始化
+void kinit() //由于我们拥有了多个锁，自然需要对多个锁同时进行初始化
 {
-  for(int i=0;i<NCPU;i++)
+  for (int i = 0; i < NCPU; i++)
   {
-    initlock(&kmem[i].lock,"kmem");
+    initlock(&kmem[i].lock, "kmem");
     //可以将所有锁命名为kmem。
   }
   // initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
-  //kinit()调用freerange()来把空闲内存页加到链表里，freerange()则是通过调用kfree()把每个空闲页（地址范围从pa_start至pa_end）逐一加到链表里来实现此功能的
+  freerange(end, (void *)PHYSTOP);
+  // kinit()调用freerange()来把空闲内存页加到链表里，freerange()则是通过调用kfree()把每个空闲页（地址范围从pa_start至pa_end）逐一加到链表里来实现此功能的
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -49,8 +49,7 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   /*用于释放指定的物理内存页，将其添加至freelist中，
   参数pa为需要释放的物理页页号，即物理页的首地址，它被看作一个没有类型的指针。
@@ -58,24 +57,23 @@ kfree(void *pa)
   因为空闲页里什么都没有，所以结构体run的成员可以直接 保存在空闲页自身里 。*/
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
   //首先将 void *pa 开始的物理页的内容全部置为1
-  r = (struct run*)pa;
+  r = (struct run *)pa;
   //参考源代码针对多个锁的情况进行修改
-  push_off();// disable interrupts to avoid deadlock.
-  int id=cpuid();
+  push_off(); // disable interrupts to avoid deadlock.
+  int id = cpuid();
   acquire(&kmem[id].lock);
   //获取锁为了操作freelist
-  r->next=kmem[id].freelist;
+  r->next = kmem[id].freelist;
   //将这空闲页物理内存加到链表头。
-  kmem[id].freelist=r;
+  kmem[id].freelist = r;
   release(&kmem[id].lock);
-  pop_off();//恢复中断
-  
+  pop_off(); //恢复中断
 
   // acquire(&kmem.lock);
   // r->next = kmem.freelist;
@@ -89,35 +87,35 @@ kfree(void *pa)
 // Returns 0 if the memory cannot be allocated.
 void *
 kalloc(void)
-{/*void* kalloc(void *)用来分配内存物理页，功能很简单，
-就是移除并返回空闲链表头的第一个元素，即给调用者分配1页物理内存。*/
+{ /*void* kalloc(void *)用来分配内存物理页，功能很简单，
+ 就是移除并返回空闲链表头的第一个元素，即给调用者分配1页物理内存。*/
   struct run *r;
-  push_off();// disable interrupts to avoid deadlock.
-  int id=cpuid();
+  push_off(); // disable interrupts to avoid deadlock.
+  int id = cpuid();
   acquire(&kmem[id].lock);
   //每次操作kmem.freelist时都需要先申请kmem.lock，此后再进行内存页面的操作。
-  r=kmem[id].freelist;
+  r = kmem[id].freelist;
   //当前CPU freelist为空时，从其他CPU freelist处获取
-  if(r)//当前CPU不为空
+  if (r) //当前CPU不为空
   {
-    kmem[id].freelist=r->next;//取走表头第一个元素
+    kmem[id].freelist = r->next; //取走表头第一个元素
   }
-  else//当前为空
+  else //当前为空
   {
-    for(int i=0;i<NCPU;i++)
+    for (int i = 0; i < NCPU; i++)
     {
-      if(i!=id)//确保从其他CPU中获取
+      if (i != id) //确保从其他CPU中获取
       {
         acquire(&kmem[i].lock);
         //还是先获取锁
-        r=kmem[i].freelist;
-        if(r)
+        r = kmem[i].freelist;
+        if (r)
         {
-          kmem[i].freelist=r->next;//获取内存
-          release(&kmem[i].lock);//解锁并结束for循环
+          kmem[i].freelist = r->next; //获取内存
+          release(&kmem[i].lock);     //解锁并结束for循环
           break;
         }
-        else//被遍历的CPU也没有空间，解锁以后继续遍历
+        else //被遍历的CPU也没有空间，解锁以后继续遍历
         {
           release(&kmem[i].lock);
           //总之得解锁
@@ -126,7 +124,7 @@ kalloc(void)
     }
   }
   release(&kmem[id].lock);
-  pop_off();//恢复中断
+  pop_off(); //恢复中断
 
   // acquire(&kmem.lock);
   // r = kmem.freelist;
@@ -134,7 +132,7 @@ kalloc(void)
   //   kmem.freelist = r->next;
   // release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
