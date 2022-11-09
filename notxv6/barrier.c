1d5b31d4df7cdf3138f1c2eb20bf0b2ae85780ec
chenyangzhu1 <chenyangzhu.cs@foxmail.com>
2022-11-09 13:47:44 +0000

    thread_done

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,24 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  //进来首先进程数+1
+  if(bstate.nthread<nthread)
+  {//如果还没有满
+  //睡一会儿
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  else
+  {//满了
+  //轮数+1，清零进程数，广播出去
+    bstate.round++;
+    bstate.nthread=0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
+
 }
 
 static void *
